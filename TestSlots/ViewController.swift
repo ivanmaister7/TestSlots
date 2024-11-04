08442e4e18c0e1dc07c222ee32de3b7d8f4de179
ivanmaister7 <ivanmaister7@gmail.com>
2024-11-05 00:27:47 +0200

    Initial Commit

---
+++ TestSlots/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSlots
+//
+//  Created by ivan on 05.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
